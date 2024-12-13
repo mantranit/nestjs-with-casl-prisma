f1d9d7780cdb60d7e19f918bba39308c5a0f2419
Man Tran <tranduyminhman@gmail.com>
2024-12-13 16:48:43 +0700

    update auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UsersModule } from 'src/users/users.module';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
